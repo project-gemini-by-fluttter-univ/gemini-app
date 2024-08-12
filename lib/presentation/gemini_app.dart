2b1209ab28fd1f11b1a6e9e68733c3f23e2c2dff
USER <user@USERnoMacBook-Pro.local>
2024-08-12 18:14:25 +0900

    kiyomizunomiyaへ名称変更

--- lib/presentation/gemini_app.dart
+++ lib/presentation/gemini_app.dart
@@ -8,7 +8,7 @@ class GeminiApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("shimizumiya"),
+          child: Text("kiyomizunomiya"),
         ),
       ),
     );
