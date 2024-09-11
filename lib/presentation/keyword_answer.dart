157bb0c1fb191d21a4f2bc2b398259f776928953
ntsts03 <ntsts03@gmail.com>
2024-09-11 12:34:58 +0900

    TextFieldからContainerへ変更

--- lib/presentation/keyword_answer.dart
+++ lib/presentation/keyword_answer.dart
@@ -8,7 +8,7 @@ class KeywordAnswer extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Padding(
-          padding: EdgeInsets.all(32.0),
+          padding: const EdgeInsets.all(32.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -20,24 +20,19 @@ class KeywordAnswer extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     color: Colors.lightBlue),
               ),
-              TextField(
-                controller: TextEditingController(
-                  text: '富士山頂で釣り！？標高3776mで何が釣れるの？まさか、雲とか？ #富士山 #釣り #妄想旅行',
+              Container(
+                height: 300,
+                width: 300,
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.lightBlue),
                 ),
-                minLines: 10,
-                maxLines: null,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.lightBlue, width: 2.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.lightBlue, width: 2.0),
-                  ),
-                ),
+                child: const Text(
+                    '富士山頂で釣り！？標高3776mで何が釣れるの？まさか、雲とか？ #富士山 #釣り #妄想旅行'),
               ),
               ElevatedButton(
                 onPressed: () {},
-                child: Text('TOPへ戻る'),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.lightBlue,
                   foregroundColor: Colors.white,
@@ -45,6 +40,7 @@ class KeywordAnswer extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
+                child: const Text('TOPへ戻る'),
               ),
             ],
           ),
