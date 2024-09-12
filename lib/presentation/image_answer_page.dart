0a6cf7fd53ae2bb27e36fea719ddec3b80c4a36d
Motoki0418 <wm1864@icloud.com>
2024-09-12 20:29:55 +0900

    画像ANSWER画面の作成
    画像以外が完成

---
+++ lib/presentation/image_answer_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class ImageAnswerPage extends StatelessWidget {
+  const ImageAnswerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'バズる文章はこちら！',
+                style: TextStyle(
+                    fontSize: 29,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.lightBlue
+                ),
+              ),
+              Container(
+                height: 107,
+                width: 320,
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.lightBlue, width: 1),
+                ),
+                child: const Text(
+                    '『ふふ、このソファ、俺専用だからね。』とでも言わんばかりの余裕っぷり。',
+                  style: TextStyle(
+                    fontSize: 19
+                  ),
+                )
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(150, 50),
+                  backgroundColor: Colors.lightBlue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: const Text(
+                    'TOPへ戻る',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
