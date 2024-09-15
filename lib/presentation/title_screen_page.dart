ab1abc25669a47cb1cc01d986e621b3b5f99c7da
清水宮榮照 <1999angolmois@gmail.com>
2024-09-15 20:21:05 +0900

    作業完了

---
+++ lib/presentation/title_screen_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class GeminiApp extends StatelessWidget {
+  const GeminiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(git checkout
+        body: SizedBox(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  "あなたのバズをお助け！",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Text(
+                  "BUZZ-AI",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "キーワードでバズる",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(240, double.infinity),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      backgroundColor: Colors.blue)),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    '画像でバズる',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(240, double.infinity),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      backgroundColor: Colors.blue)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
