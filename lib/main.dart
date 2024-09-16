c4e73ed9ca1dfb1503a97fdde9885ee8d0892be3
VisitSpace-com <hiroyuki.1995.09.09@icloud.com>
2024-09-16 22:58:51 +0900

    画像選択の画面を作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gemini_app/presentation/gemini_app.dart';
-import 'package:gemini_app/presentation/select_photo_page.dart';
 
 void main() {
-  runApp(const SelectPhotoPage());
+  runApp(const GeminiApp());
 }
