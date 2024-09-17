2395c4eb7e9d4efa357a1b93b986a9c69cf5ee55
VisitSpace-com <hiroyuki.1995.09.09@icloud.com>
2024-09-18 02:36:03 +0900

    キーワード入力画面を作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gemini_app/presentation/gemini_app.dart';
-import 'package:gemini_app/presentation/keyword_input_page.dart';
 
 void main() {
-  runApp(const KeywordInputPage());
+  runApp(const GeminiApp());
 }
